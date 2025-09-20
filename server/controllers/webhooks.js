92ba642844860544340713b13c3f454ec9a75f66
Abhishek S A <abhishekangadi2050@gmail.com>
2025-09-20 16:52:21 +0530

    Updated filename with new changes

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -1,56 +1,60 @@
 import Stripe from "stripe";
 import Transaction from "../models/Transaction.js";
-import User from '../models/User.js';
-
+import User from "../models/User.js";
 
 export const stripeWebhooks = async (request, response) => {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+  const sig = request.headers["stripe-signature"];
 
-    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
-    const sig = request.headers["stripe-signature"]
+  let event;
 
-    let event;
-        
-    try {
-        event = stripe.webhooks.constructEvent(request.body, sig, process.env.STRIPE_WEBHOOK_SECRET)
-    } catch (error) {
-        return response.status(400).send(`webhook error: ${error.message}`)
-    }
+  try {
+    event = stripe.webhooks.constructEvent(
+      request.body, // must be raw body
+      sig,
+      process.env.STRIPE_WEBHOOK_SECRET
+    );
+  } catch (error) {
+    return response.status(400).send(`Webhook error: ${error.message}`);
+  }
 
-    try {
-        switch (event.type) {
-            case "payment_intent.succeeded":{
-                const paymentIntent = event.data.object;
-                const sessionList = await stripe.checkout.sessions.list({
-                    paymentIntent: paymentIntent.id,
-                })
+  try {
+    switch (event.type) {
+      case "checkout.session.completed": {
+        const session = event.data.object;
+        const { transactionId, appId } = session.metadata || {};
 
-                const session = sessionList.data[0];
-                const { transactionId, appId } = session.metadata;
+        if (appId === "quickgpt") {
+          const transaction = await Transaction.findOne({
+            _id: transactionId,
+            isPaid: false,
+          });
 
-                if(appId = 'quickgpt'){
-                    const transaction = await Transaction.findOne({_id: transactionId, isPaid: false})
+          if (transaction) {
+            // Add credits to user
+            await User.updateOne(
+              { _id: transaction.userId },
+              { $inc: { credits: transaction.credits } }
+            );
 
-                    // update credits in user account
-                    await User.updateOne({_id: transaction.userId}, {$inc: {credits: transaction.credits}})
-
-                    //Update credit payment status
-                    transaction.isPaid = true;
-                    await transaction.save();
-
-                }else{
-                    return response.json({received: true, message: "Ignored event: Inavalid app"})
-                }
-            }
-                
-                break;
-        
-            default:
-                console.log("Unhandled event type: ", event.type )
-
-                break;
+            // Mark transaction as paid
+            transaction.isPaid = true;
+            await transaction.save();
+          }
+        } else {
+          console.log("Ignored event: Invalid app");
         }
-    } catch (error) {
-        console.error("Webhook processing error:", error)
-        response.status(500).send("Internal server error")
+        break;
+      }
+
+      default:
+        console.log("Unhandled event type:", event.type);
+        break;
     }
-}
+
+    response.json({ received: true });
+  } catch (error) {
+    console.error("Webhook processing error:", error);
+    response.status(500).send("Internal server error");
+  }
+};
